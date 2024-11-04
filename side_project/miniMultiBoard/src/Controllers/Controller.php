438920299573cca31454a14f4f2e9acbf1ed5a1c
ByungjooPark <milkpbj23@gmail.com>
2024-11-04 17:45:51 +0900

    241104  mvc start

---
+++ side_project/miniMultiBoard/src/Controllers/Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Controllers;
+
+class Controller {
+    // 생성자
+    public function __construct(string $action) {
+        // 세션 시작
+
+        // 유저 로그인 및 권한체크
+
+        // 해당 Action 호출
+        $resultAction = $this->$action();
+
+        // view 호출
+        $this->callView($resultAction);
+
+        exit; // php 처리 종료
+    }
+
+    /**
+     * 뷰 OR 로케이션 처리용 메소드
+     */
+    private function callView($path) {
+        if(strpos($path, 'Location:') === 0) {
+            header($path);
+        } else {
+            require_once(_PATH_VIEW.'/'.$path);
+        }
+    }
+}
