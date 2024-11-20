018b2832c7e7657fa1438ffe7d105cf3dcfaf4e6
ByungjooPark <milkpbj23@gmail.com>
2024-11-20 14:55:15 +0900

    241120 뷰스타그램 ~ 뷰 구현

---
+++ side_project/vuestagram/resources/views/components/board/BoardListComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- 리스트 -->
+    <div class="board-list-box">
+        <div @click="openModal" class="item">
+            <img src="/img/meerkat0.png">
+        </div>
+        <div @click="openModal" class="item">
+            <img src="/img/meerkat1.png">
+        </div>
+        <div @click="openModal" class="item">
+            <img src="/img/meerkat2.png">
+        </div>
+        <div @click="openModal" class="item">
+            <img src="/img/meerkat3.png">
+        </div>
+        <div @click="openModal" class="item">
+            <img src="/img/meerkat4.png">
+        </div>
+    </div>
+
+    <!-- 상세 모달 -->
+    <div v-show="modalFlg" class="board-detail-box">
+        <div class="item">
+            <img src="/img/meerkat0.png">
+            <hr>
+            <p>내용내용</p>
+            <hr>
+            <div class="etc-box">
+                <span>작성자 : 김아무개</span>
+                <button @click="closeModal">닫기</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// ------------------------------
+// 모달 관련
+const modalFlg = ref(false);
+const openModal = () => {
+    modalFlg.value = true;
+}
+const closeModal = () => {
+    modalFlg.value = false;
+}
+// ------------------------------
+
+</script>
+
+<style>
+@import url('../../../css/boardList.css');
+</style>
