7e1780cff0bd942e5a222fd697dd687fb0372088
ByungjooPark <milkpbj23@gmail.com>
2024-11-27 16:53:36 +0900

    241127 board com

--- side_project/vuestagram/app/Http/Controllers/BoardController.php
+++ side_project/vuestagram/app/Http/Controllers/BoardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Board;
 use Illuminate\Http\Request;
+use MyToken;
 
 class BoardController extends Controller
 {
@@ -14,6 +15,42 @@ class BoardController extends Controller
             'success' => true
             ,'msg' => '게시글 획득 성공'
             ,'boardList' => $boardList->toArray()
+        ];
+
+        return response()->json($responseData, 200);
+    }
+
+    public function show($id) {
+        // $board = Board::join('users', 'boards.user_id', '=', 'users.user_id')
+        //                 ->select('boards.*', 'users.name')
+        //                 ->find($id);
+        $board = Board::with('user')->find($id);
+        
+        $responseData = [
+            'success' => true
+            ,'msg' => '상세 정보 획득 성공'
+            ,'board' => $board->toArray()
+        ];
+
+        return response()->json($responseData, 200);
+    }
+
+    public function store(Request $request) {
+        // TODO 유효성체크 넣어주세요.
+
+        // insert data 생성
+        $insertData = $request->only('content');
+        $insertData['user_id'] = MyToken::getValueInPayload($request->bearerToken(), 'idt');
+        $insertData['like'] = 0;
+        $insertData['img'] = '/'.$request->file('file')->store('img');
+
+        // insert
+        $board = Board::create($insertData);
+
+        $responseData = [
+            'success' => true
+            ,'msg' => '게시글 작성 성공'
+            ,'board' => $board->toArray()
         ];
 
         return response()->json($responseData, 200);
